059b590de46cd50fe40936f012f445a9300060c5
Edwin <edwinschutjes@gmail.com>
2024-11-15 14:12:35 +0100

    add more router methods

---
+++ app/(dashboard)/auth/page.tsx
@@ -0,0 +1,11 @@
+const AuthPage = () => {
+  return (
+    <p>
+      This is a route group example. Since (dashboard) is stated within
+      parentheses, the Auth page can be accessed directly in the URL and
+      dashboard will not show up/be needed within the browser route. eg:
+      domain.com/auth.
+    </p>
+  );
+};
+export default AuthPage;
---
+++ app/_private/Example.md
@@ -0,0 +1,1 @@
+This is just an example of a private folder in Next. Using underscore before the folder name prevents it from showing up as a route.
